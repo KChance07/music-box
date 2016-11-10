015061c34431552202ab231787202b0f52688b1f
Kyle Chance <kylechance@gmail.com>
2016-11-09 20:58:07 -0500

    built out index

---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var server = express();
+var mongoose = require('mongoose');
+var todoRouter = require(./server/routers/todo.router.js);
+var mongoURI = process.env.MONGOURI || require('./config.js').mongoURI;
+
+mongoose.connect(mongoURI);
+
+var port = process.env.PORT || 8080;
+
+server.use(express.static(__dirname + '/public'));
+
+server.get('/', function(req, res){
+  res.sendFile('public/html/index.html', {root:__dirname});
+});
+
+server.use(todoRouter);
+
+server.listen(port, function(){
+  console.log('Now listening on port...', port);
+});
---
+++ public/js/app.js
@@ -0,0 +1,5 @@
+(function() {
+
+  angular.module('music-box', []);
+
+}());
